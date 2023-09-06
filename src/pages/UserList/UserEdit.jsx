e9904fc2f9c36c7aeb12600e828623405452dd61
Francisco Fernandez <ffernandez@apside.pt>
2023-09-06 11:29:03 +0100

    add hook form and zod validation

--- src/pages/UserList/UserEdit.jsx
+++ src/pages/UserList/UserEdit.jsx
@@ -1,62 +1,89 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { get, put } from  '@/axios/axios.js'
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { get, put } from '@/axios/axios.js';
 
 const UserEdit = () => {
    const { id } = useParams();
-   const [user, setUser] = useState({});
+
+   // Mantén una copia del objeto user original
+   const [originalUser, setOriginalUser] = useState({
+      firstName: '',
+      lastName: '',
+      email: '',
+   });
+
+   const schema = z.object({
+      firstName: z.string().min(2).max(20),
+      lastName: z.string().min(2).max(20),
+      email: z.string().email(),
+   });
+
+   const { register, handleSubmit, formState: { errors }, setValue } = useForm({
+      resolver: zodResolver(schema),
+   });
 
    useEffect(() => {
+      get(`users/${id}`)
+         .then((response) => {
+         // Almacena el usuario original en originalUser
+         setOriginalUser(response);
 
-      get(`users/`+id)
-      .then((response) => {
-         setUser(response);
-      })
-      .catch((error) => {
-            console.log(error);
-      });
-
-   }, []);
+         // Establece los valores del formulario solo para las propiedades específicas
+         setValue('firstName', response.firstName);
+         setValue('lastName', response.lastName);
+         setValue('email', response.email);
+         })
+         .catch((error) => {
+         console.log(error);
+         });
+   }, [id, setValue]);
 
    const navigate = useNavigate();
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      put(`users/`+id, user)
-      .then(res => {
+   const submitData = (formData) => {
+      // Combina las propiedades actualizadas con las propiedades originales
+      const updatedUser = {
+         ...originalUser,
+         ...formData,
+      };
+
+      put(`users/${id}`, updatedUser)
+         .then(() => {
          navigate('/users');
-      })
-      .catch((error) => {
+         })
+         .catch((error) => {
          console.log(error);
-      });
-   }
-   
+         });
+   };
 
    return (
       <>
-            <div className="grid justify-items-center bg-gray-100">
-            <div className=" w-[1000px] mt-8 p-8 rounded-lg">
-               <div className="mt-5 md:mt-0 ">
-                  
-                  <form onSubmit={handleSubmit}>
+         <div className="grid justify-items-center bg-gray-100">
+            <div className="w-[1000px] mt-8 p-8 rounded-lg">
+               <div className="mt-5 md:mt-0">
+                  <form onSubmit={handleSubmit(submitData)}>
                   <div className="shadow overflow-hidden sm:rounded-md">
                      <div className="px-4 py-5 bg-white sm:p-6">
                         <div className="grid grid-cols-6 gap-6">
                         <div className="col-span-6 sm:col-span-3">
                            <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">First name</label>
-                           <input type="text" name="first_name" id="first_name" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" 
-                              value={user.firstName} onChange={e => setUser({...user, firstName: e.target.value})}
-                           />
+                           <input type="text" name="first_name" id="first_name" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" {...register('firstName')} />
+                           {errors.firstName && <span className="text-red-500 text-xs">{errors.firstName.message}</span>}
                         </div>
 
                         <div className="col-span-6 sm:col-span-3">
                            <label htmlFor="last_name" className="block text-sm font-medium text-gray-700">Last name</label>
-                           <input type="text" name="last_name" id="last_name" value={user.lastName} autoComplete="family-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                           <input type="text" name="last_name" id="last_name" autoComplete="family-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" {...register('lastName')} />
+                           {errors.lastName && <span className="text-red-500 text-xs">{errors.lastName.message}</span>}
                         </div>
 
                         <div className="col-span-6 sm:col-span-4">
                            <label htmlFor="email_address" className="block text-sm font-medium text-gray-700">Email address</label>
-                           <input type="text" name="email_address" id="email_address" value={user.email} autoComplete="email" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                           <input type="text" name="email_address" id="email_address" autoComplete="email" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" {...register('email')} />
+                           {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
                         </div>
 
                         <div className="col-span-6">
@@ -76,16 +103,14 @@ const UserEdit = () => {
                         </div>
                      </div>
                      <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Save
-                        </button>
+                        <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
                      </div>
                   </div>
                   </form>
                </div>
             </div>
-            </div>
-         </>
+         </div>
+      </>
    );
 };
 
